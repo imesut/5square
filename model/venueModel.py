7a4530eec507ed041d95112ca8cab7d0487b5151
Mesut Yılmaz <im.mesut.yilmaz@gmail.com>
2023-06-05 23:45:13 +0300

    venue detail updated

--- model/venueModel.py
+++ model/venueModel.py
@@ -3,85 +3,50 @@ import random
 from utils.envUtils import ENV
 
 venues = [
-    # {
-    #     "id": "1002",
-    #     "name": "The 5quare Restaurant",
-    #     "address": "Random Street, Number 3/5",
-    #     "phone": "1234-567-8901",
-    #     "score": "3.5/5",
-    #     "description": "Visual description will come here.",
-    #     "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSCMa9b83vLTLq3g5mcPHi9DLUeMhlLbyLbQ&usqp=CAU",
-    #     "menu": [
-    #         {
-    #             "id": "3001", 
-    #             "name": "Pie Lemon Meringue",
-    #             "price": 2.15,
-    #             "currency": "USD",
-    #             "3DModel": "https://developer.apple.com/augmented-reality/quick-look/models/meringue/pie_lemon_meringue.usdz",
-    #             "image": "https://www.onceuponachef.com/images/2018/04/Lemon-Meringue-Pie-1.jpg"
-    #         },
-    #         {
-    #             "id": "3002", 
-    #             "name": "Burger Awesome",
-    #             "price": 7.49,
-    #             "currency": "USD",
-    #             "3DModel": "",
-    #             "image": "https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/102cf51c-9220-4278-8b63-2b9611ad275e/Derivates/3831dbe2-352e-4409-a2e2-fc87d11cab0a.jpg"
-    #         },
-    #         {
-    #             "id": "3003",
-    #             "name": "Lemonade",
-    #             "price": 1.49,
-    #             "currency": "USD",
-    #             "3DModel": "",
-    #             "image": "https://www.simplyrecipes.com/thmb/4LFrc9hSMoKErr2WI7tThcnvWwA=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Simply-Recipes-Perfect-Lemonade-LEAD-08-B-441ceb568f854bb485dbed79e082bb4a.jpg"
-    #         }
-    #     ]
-    # },
     {
         "id": "1003",
-        "name": "Chief's Pizza",
-        "address": "Awesome Street, Number 5/5",
+        "name": "Coffee Public",
+        "address": "Coffee Street Number 5/5",
         "score": "3.5/5",
         "phone": "9876543210",
-        "description": "Visual description will come here.",
-        "image": "https://d332juqdd9b8hn.cloudfront.net/wp-content/uploads/2019/04/BEAUTIFULPIZZERIA.jpg",
+        "description": "Coffee Public is a small sweet spot located on a wide street, with a few small tables in front and a cash register on the right side upon entering. In front of coffee, you'll find a cozy atmosphere with around 3-4 tables. It doesn't appear crowded based on the photos. The place mainly specializes in desserts and coffee, which are the standout items.",
+        "image": "https://github.com/atakan-nalbant/hackathonimages/blob/main/coffeepublic.jpg?raw=true",
         "menu": [
             {
                 "id": "4001",
-                "name": "Pizza Margherita (Gluten Free)",
-                "price": 12.50,
+                "name": "Lemon Cheesecake",
+                "price": 7.99,
                 "currency": "USD",
                 "3DModel": "",
-                "image": "https://cookieandkate.com/images/2021/07/classic-margherita-pizza.jpg",
+                "image": "https://github.com/atakan-nalbant/hackathonimages/blob/main/Lemon%20Cheesecake.jpg?raw=true",
                 "gluten_free": True,
                 "lactose_free": False,
-                "vegetarian": False,
-                "ingredients": "A, B, C, D"
+                "vegetarian": True,
+                "ingredients": "Cream cheese, Digestive biscuit crust, Lemon zest, Lemon juice"
             },
             {
                 "id": "4002",
-                "name": "Pizza Margherita",
-                "price": 12.50,
+                "name": "Chocolate Chip Cookie",
+                "price": 14.99,
                 "currency": "USD",
                 "3DModel": "",
-                "image": "https://cookieandkate.com/images/2021/07/classic-margherita-pizza.jpg",
+                "image": "https://github.com/atakan-nalbant/hackathonimages/blob/main/Chocolate%20Chip%20Cookie.jpg?raw=true",
                 "gluten_free": False,
                 "lactose_free": False,
-                "vegetarian": False,
-                "ingredients": "A, B, C, D"
+                "vegetarian": True,
+                "ingredients": "Chocolate Chips, Butter, Gluten Free Flour, Brown sugar, White sugar, Sea Salt"
             },
             {
                 "id": "4003",
-                "name": "Pizza Pepperoni",
-                "price": 17.50,
+                "name": "Latte",
+                "price": 4.99,
                 "currency": "USD",
-                "3DModel": "",
-                "image": "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F19%2F2014%2F07%2F10%2Fpepperoni-pizza-ck-x.jpg&q=60",
-                "gluten_free": False,
-                "lactose_free": False,
-                "vegetarian": False,
-                "ingredients": "A, B, C, D"
+                "3DModel": "https://raw.githubusercontent.com/atakan-nalbant/hackathonimages/main/latte%203d.usdz",
+                "image": "https://github.com/atakan-nalbant/hackathonimages/blob/main/latte%202.jpg?raw=true",
+                "gluten_free": True,
+                "lactose_free": True,
+                "vegetarian": True,
+                "ingredients": "Espresso shot, steamed milk, frothed milk."
             }
         ]
     }
